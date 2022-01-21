0a67d95370d4a09ddba6cca3055bd68af9455d73
Drescher <vianney.drescher@accenture.com>
2022-01-21 15:32:35 +0100

    Mise à jour du format du mail de notification

--- modules/mail/mail.py
+++ modules/mail/mail.py
@@ -10,6 +10,8 @@ import smtplib, ssl
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
+# mise en forme de tableaux
+from tabulate import tabulate
 
 
 # retourne l'emplacement du contenu html de l'email genere pour la semaine passée en paramètre
@@ -46,8 +48,22 @@ def agregate_modalite(df_commandes_in, semaine_in) :
     df_ret.sort_values(by=["typePS","vaccin","modaliteCommande"], inplace=True)
     return df_ret
 
+# generation du tableau de mail
+def commandes_to_table_mail(df_in, semaine_in) :
+    df_table = df_in[df_in["dateCommande"] == datetime.datetime.strptime(semaine_in, "%Y-%m-%d")]
+    df_table = df_table.groupby(by=["vaccin","typePS"], as_index=False, dropna=False)["nombreArticle"].sum()
+    df_table.rename(columns = {"vaccin" : "Vaccin", 
+        "typePS" : "Catégorie PS",
+        "nombreArticle" : "Nombre flacons" }, inplace=True)
+    df_table = pd.pivot_table(df_table, values = ["Nombre flacons"], 
+        columns = ["Vaccin"], index = ["Catégorie PS"],
+        fill_value=0)
+    df_table.columns = df_table.columns.map('-'.join).str.strip('-')
+    table_html = df_table.to_html()
+    return table_html
+
 # generation du corpus de texte à partir de jinja2
-def generate_resume_df(df_commandes_modalite, semaine_in, L_config_domaine) :
+def generate_resume_df_old(df_commandes_modalite, semaine_in, L_config_domaine) :
     # templates utilises plus base
     template_header = "<h2>Résumé de l'opération de commande du {{semaine}}</h2>"
     template_line = "<li>{{modalite}} : {{nb_modalite}} commandés par {{nb_PS}} {{typePS}}</li>"
@@ -100,6 +116,57 @@ def generate_resume_df(df_commandes_modalite, semaine_in, L_config_domaine) :
     html_ret = html_ret + '\n' + "</body>\n</html>"
     return html_ret
 
+# fonction pour calculer l'augmentation en % entre deux semaines.
+# retourne un string
+def calc_aug_relative_s1s2(nb_s1, nb_s2) :
+    if (nb_s1 == 0) or (nb_s2 == 0) :
+        return ""
+    else :
+        incr = round((nb_s2 - nb_s1)*100/nb_s1, 2)
+        if incr >= 0 :
+            return "+" + str(incr) + " %"
+        else :
+            return str(incr) + " %"
+
+
+# génération du corpus de texte du mail en html
+def generate_resume_df(df_commandes, semaine_in, L_config_domaine) :
+    #preparation
+    last_semaine = datetime.datetime.strptime(semaine_in, "%Y-%m-%d") - datetime.timedelta(7)
+    table_mail = commandes_to_table_mail(df_commandes, semaine_in)
+
+    # templates utilises plus base
+    
+    template_header = "<h3>Résumé de l'opération de commande du {{semaine}}</h3>"
+    template_subheader = "<p> Commandes {{vaccin}} : {{nb_flacons_vaccin_cur}} flacons commandés cette semaine vs {{nb_flacons_vaccin_last}} la semaine précédente. {{aug_relative}} </p>"
+    # generation de la chaine de texte
+    header = Template(template_header).render({"semaine" : semaine_in})
+    html_ret = "<!DOCTYPE html>\n<html>\n<body>"
+    html_ret = html_ret + '\n' + header
+
+    # sous paragraphe pour chaque vaccin 
+    html_ret = html_ret + '\n' + "<ul>"
+    for vaccin in df_commandes["vaccin"].unique() :
+        html_ret = html_ret + '\n'
+        nb_flacons_vaccin_cur = df_commandes.loc[
+            (df_commandes["vaccin"] == vaccin) & 
+            (df_commandes["dateCommande"] == datetime.datetime.strptime(semaine_in, "%Y-%m-%d")), "nombreArticle"].sum()
+        nb_flacons_vaccin_last = df_commandes.loc[
+            (df_commandes["vaccin"] == vaccin) & 
+            (df_commandes["dateCommande"] == last_semaine), "nombreArticle"].sum()
+        subheader = Template(template_subheader).render({"vaccin" : vaccin, 
+                                                        "nb_flacons_vaccin_cur" : nb_flacons_vaccin_cur,
+                                                        "nb_flacons_vaccin_last" : nb_flacons_vaccin_last,
+                                                        "aug_relative" : calc_aug_relative_s1s2(nb_flacons_vaccin_last, nb_flacons_vaccin_cur)})
+        html_ret = html_ret + subheader + '\n'
+    html_ret = html_ret + "</ul>" + '\n'
+    # sous paragraphe pour le tableau
+    html_ret = html_ret + '\n' + "<h3>Tableau récapitulatif : </h3>" + '\n'
+    html_ret = html_ret + '\n' + table_mail
+
+    html_ret = html_ret + '\n' + "</body>\n</html>"
+    return html_ret
+
 def save_html(html_in, folder_out, semaine_in) :
     Html_file= open(os.path.join(folder_out, semaine_in + "-PHARMA_SI_RESUME_COMMANDES.html"),"w", encoding="utf-8")
     Html_file.write(html_in)
@@ -108,8 +175,7 @@ def save_html(html_in, folder_out, semaine_in) :
 
 def generate_html(combined_name_in, semaine_in, L_config_domaine, folder_out='corpus_mails', folder_in="fichiers_consolidés") :
     df_commandes = load_commandes(folder_in, combined_name_in)
-    df_commandes_modalite = agregate_modalite(df_commandes, semaine_in)
-    html_commandes  = generate_resume_df(df_commandes_modalite, semaine_in, L_config_domaine)
+    html_commandes  = generate_resume_df(df_commandes, semaine_in, L_config_domaine)
     save_html(html_commandes, folder_out, semaine_in)
     return
 
